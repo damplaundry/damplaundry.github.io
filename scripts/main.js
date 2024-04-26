2beb287bda711a908abb91ae425239972d6a2544
damplaundry <144713235+damplaundry@users.noreply.github.com>
2024-04-26 17:25:20 +0000

    aaa

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,16 @@
-//taskone
+console.log("Task 1")
 for (let i = 0; i <=10; i++) {
     console.log(i*i);
     
 }
 
-//tasktwo
+console.log("Task 2")
 for (let i = 5; i > 0; i--) {
     console.log(i);
-    
+}
+
+console.log("Task 2
+")
+for (let i = 50; i <= 50; i--) {
+    console.log(2 * 1);
 }
