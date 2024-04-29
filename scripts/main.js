cf857ba26dbaee72acf5b64bb1520db99dace886
damplaundry <144713235+damplaundry@users.noreply.github.com>
2024-04-29 16:12:12 +0000

    tasks 5

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,31 @@ for (let i = 0; i <=10; i++) {
 console.log("Task 2")
 for (let i = 5; i > 0; i--) {
     console.log(i);
+    console.log("BLAST OFF")
+}
+
+
+console.log("Task 3")
+for (let i = 1; i <=50; i+=2) {
+    console.log(i+1);
+    
+}
+
+console.log("Task 4")
+let sum = 0
+for (let i = 1; i <= 100; i++) {
+   sum = sum + i
+}
+console.log(sum) 
+
+console.log("Task 5")
+let factorial = 1;
+for (let i = 1; i <= 10; i++){
+   factorial *= i;
+   console.log(factorial);
+}
+
+console.log("Task 6")
+for (let i = 0; i < array.length; i++) {
+    let arr = [3, 1, 4, 1, 5];
 }
