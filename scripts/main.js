6e5a51a7b08dac65e9c85ac394cf0e32725a08e1
damplaundry <144713235+damplaundry@users.noreply.github.com>
2024-04-30 01:36:48 +0000

    tasks 1-9

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,9 @@ for (let i = 0; i <=10; i++) {
 console.log("Task 2")
 for (let i = 5; i > 0; i--) {
     console.log(i);
-    console.log("BLAST OFF")
 }
 
+console.log("BLAST OFF")
 
 console.log("Task 3")
 for (let i = 1; i <=50; i+=2) {
@@ -31,7 +31,33 @@ for (let i = 1; i <= 10; i++){
    console.log(factorial);
 }
 
-console.log("Task 6")
+console.log("Task 7")
+const arr = [3, 1, 4, 1, 5];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr.at(i));
+}
+
+console.log("Task 8")
+
+let cubes = [];
+
+for (let i = 1; i <= 10; i++) {
+  cubes.push(i**3);
+}
+console.log(cubes);
+
+console.log("Task 9")
+let fib = [0, 1]
+
+for (let i = 1; i <= 10; i++) {
+  fib.push(fib.at(-1) + (fib.at(-2)));
+}
+console.log(fib);
+
+console.log("Task 10");
+let empty = []
+
 for (let i = 0; i < array.length; i++) {
-    let arr = [3, 1, 4, 1, 5];
+  
 }
