7e9b1da1f9b9d81ad7f2698e238d04ee3eed61ca
damplaundry <144713235+damplaundry@users.noreply.github.com>
2024-05-06 15:54:53 +0000

    creating dynamic content

--- scripts/poopoo.js
+++ scripts/poopoo.js
@@ -1,1 +1,8 @@
-let arr = ["mediumblue", "mediumpurple", "mediumslateblue", "mediumspringgreen", "mediumseagreen", "maroon", "powderblue"]
+let color = ["mediumblue", "mediumpurple", "mediumslateblue", "mediumspringgreen", "mediumseagreen", "maroon", "powderblue"];
+list = document.createElement('ul');
+
+console.log("colors")
+for (let i = 0; i <=7; i++) {
+    console.log(color);
+    
+}
